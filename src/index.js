b19625b4608aa3d1df88a2833373754ef4fec5c7
wojtek534 <wojtek.rzepkaa@gmail.com>
2018-02-10 11:00:46 +0100

    0.22

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 require('animate.css');
+
+const $ = require('jquery');
+const Tether = require('tether');
+const Popper = require('popper.js');
+//
+window.jQuery = window.$ = $;
+window.Tether = Tether;
+window.Popper = Popper;
+//
+require('bootstrap');
